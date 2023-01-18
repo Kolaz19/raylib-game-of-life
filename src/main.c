9e9c2f8d73e5d4e7e048c5eaaa6b0ec9ce476b6b
Nikolas <etc.nikolas@gmail.com>
2023-01-18 21:18:06 +0100

    Prepare background texture

--- src/main.c
+++ src/main.c
@@ -1,19 +1,23 @@
 #include "../include/raylib.h"
 
-
+#define BACKGROUND_SIZE 3000
 
 void zoomCam(Camera2D *cam);
+void prepareBackgroundTexture(RenderTexture2D *texture);
 
 int main(void)
 {
     // Initialization
     const int screenWidth = 1920*0.7f;
     const int screenHeight = 1080*0.7f;
-    Rectangle background = {0,0,3000,3000};
+    InitWindow(screenWidth, screenHeight, "Game of Life");
+
+    RenderTexture2D backgroundTexture = LoadRenderTexture(BACKGROUND_SIZE,BACKGROUND_SIZE);
+    prepareBackgroundTexture(&backgroundTexture);
     Camera2D cam = {0};
     cam.zoom = 1.0f;
 
-    InitWindow(screenWidth, screenHeight, "Game of Life");
+
 
     SetTargetFPS(10);              
     //--------------------------------------------------------------------------------------
@@ -30,10 +34,8 @@ int main(void)
 
             ClearBackground(RAYWHITE);
             BeginMode2D(cam);
-            DrawRectangle(background.x, background.y, background.width, background.height, YELLOW);
-            for(int i = 0; i < 1500; i++) {
-                DrawLine(i*2,0,i*2,1500,BLACK);
-            }
+            DrawTexture(backgroundTexture.texture,0,0,WHITE);
+
             EndMode2D();
 
         EndDrawing();
@@ -48,11 +50,22 @@ int main(void)
     return 0;
 }
 
+void prepareBackgroundTexture(RenderTexture2D *texture) {
+    const int spaceBetweenLines = 5;
+    BeginTextureMode(*texture);
+            DrawRectangle(0,0,BACKGROUND_SIZE,BACKGROUND_SIZE,YELLOW);
+            for(int i = 0; i < BACKGROUND_SIZE/spaceBetweenLines; i++) {
+                DrawLine(i*spaceBetweenLines,0,i*spaceBetweenLines,BACKGROUND_SIZE,BLACK);
+                DrawLine(0,i*spaceBetweenLines,BACKGROUND_SIZE,i*spaceBetweenLines,BLACK);
+            }
+    EndTextureMode();
+}
+
 void zoomCam(Camera2D *cam) {
     float mouseWheelMovement = GetMouseWheelMove();   
     if(mouseWheelMovement > 0) {
-        cam->zoom += 0.02f;
+        cam->zoom += 0.1f;
     } else if(mouseWheelMovement < 0) {
-        cam->zoom -= 0.02f;
+        cam->zoom -= 0.1f;
     }
 }
